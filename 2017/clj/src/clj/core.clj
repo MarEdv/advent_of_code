0cd36cac3dd578ba8816796ca63ccea4c17c7192
Marcus Edvinsson <edvinsson@gmail.com>
2017-12-02 10:21:19 +0100

    Day 2, 2017: Corruption Checksum

--- 2017/clj/src/clj/core.clj
+++ 2017/clj/src/clj/core.clj
@@ -1,9 +1,13 @@
 (ns clj.core
   (:gen-class)
-  (:require [clj.day1]))
+  (:require [clj.day1])
+  (:require [clj.day2])
+  )
 
 (defn -main
   [& args]
   (println "Day1!")
   (clj.day1/day1)
+  (println "Day2!")
+  (clj.day2/day2)
   )
