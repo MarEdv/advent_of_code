7234323ff117a31b495fa7fe195eb4a5a9144e5c
Marcus Edvinsson <edvinsson@gmail.com>
2018-01-24 21:18:15 +0100

    Day 14, 2017: Disk Defragmentation, part 2

--- 2017/clj/src/clj/day14.clj
+++ 2017/clj/src/clj/day14.clj
@@ -2,7 +2,13 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
             [clj.day10 :as day10]
-            ))
+            [clojure.set :as set]))
+
+(def test-grid [[\0 \1 \0 \1]
+                [\1 \1 \0 \0]
+                [\0 \0 \1 \0]
+                [\1 \1 \1 \1]
+                ])
 
 (defn prepareInput [input]
   (concat (map int input) [17 31 73 47 23]
@@ -29,7 +35,7 @@
   )
 
 (defn createInput [x]
-  (->> (str "ugkiagan-" (String/valueOf x))
+  (->> (str "flqrgnkx-" (String/valueOf x))
        )
   )
 
@@ -63,6 +69,63 @@
        )
   )
 
+(def directions [[0 1] [1 0] [-1 0] [0 -1]])
+
+(defn within-boarders? [grid a]
+  (and (<= 0 a)
+       (> (count grid) a)
+       )
+  )
+
+(defn walk-group [grid visited coords]
+  (if (not= (get (get grid (second coords)) (first coords)) \1)
+    [(conj visited coords) []]
+    (let [candidates (map #(map + % coords) directions)
+          validCandidates (filter #(every? (partial within-boarders? grid) %) candidates)
+          notVisitedCandidates (filter #(not (contains? visited %)) validCandidates)
+          ]
+      (if (contains? visited coords)
+        [(conj visited coords) []]
+        (reduce (fn [[v n] c] (let [[v2 n2] (walk-group grid v c)]
+                                [v2 (concat n n2)]
+                                ))[(conj visited coords) [coords]] notVisitedCandidates)
+        )
+      )
+    )
+  )
+
+(defn identifyGroups
+  [grid]
+  (loop [v (set [])
+         groups []
+         coords (for [x (range 128) y (range 128)] (vector x y))
+         ]
+    (if (empty? coords)
+      groups
+      (let [c (first coords)
+            [v2 group] (walk-group grid v c)
+            g2 (if (empty? group)
+                 groups
+                 (cons group groups)
+                 )
+            ]
+        ;(println (str c))
+        (recur (set/union v2 v) g2 (rest coords))
+        )
+      )
+    )
+  )
+
+(defn step2 []
+  (->> (calculateBinaryGrid)
+       (map vec)
+       (vec)
+       (identifyGroups)
+       (count)
+       )
+  )
+
 (defn day14 []
   (println (str "Step 1: " (step1)))
+  (println (str "Step 2: " (step2)))
   )
