5e6eafef9c871567ab103a674a24b6356cafe65d
Marcus Edvinsson <edvinsson@gmail.com>
2018-12-02 11:45:11 +0100

    Advent of Code 2018

---
+++ 2018/clj/README.md
@@ -0,0 +1,13 @@
+# Advent of Code, 2018
+These are my solutions to the Advent of Code puzzels of 2018, https://www.adventofcode.com.
+
+## Usage
+
+    $ lein run
+
+## License
+
+Copyright © 2018 Marcus Edvinsson
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ 2018/clj/project.clj
@@ -0,0 +1,12 @@
+(defproject clj "0.1.0-SNAPSHOT"
+  :description "My solutions to Advent of Code 2018"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/math.combinatorics "0.1.4"]
+                 [net.mikera/core.matrix "0.61.0"]
+                 [ubergraph "0.4.0"]]
+  :main ^:skip-aot clj.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
